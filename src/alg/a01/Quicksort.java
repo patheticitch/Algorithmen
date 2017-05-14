948dd77263e1754090712502ce0d738f5bc12432
Lena Schuster <Lena@Lenas-Air.fritz.box>
2017-05-14 15:43:03 +0200

    added complete comments

--- src/alg/a01/Quicksort.java
+++ src/alg/a01/Quicksort.java
@@ -6,22 +6,35 @@ public class Quicksort {
 	 * @param array  das zu sortierende Array
 	 * Vorbedingung: das Array muss vom Typ int[] sein
 	 * @return array  das sortierte array
-	 * Effekt: sortiert das gegebene Array
-	 */
-
+	 * Effekt -
+	 ********************************************************************************************/
 	public static int[] sort(int[] array) {
 		quicksort(array);
 		return array;
 	}
 
+	
+	/********************************************************************************************
+	 * Methode zur initialisierung des Quicksort Verfahrens im Bereich array[0]...array[length-1]
+	 * @param array  das zu sortierende Array
+	 * @return -
+	 * Effekt: Erstaufruf der rekursive Methode quicksort(array,left,right).
+	 ********************************************************************************************/
 	private static void quicksort(int[] array) {
-		// starts the quicksort
 		quicksort(array, 0, array.length - 1);
 	}
-
+	
+	
+	/********************************************************************************************
+	 * Methode quickSort sortiert das Array in den Grenzen von left bis right(inklusive)
+	 * (Verfahren rekursiv)
+	 * @param array das zu sortierende Array
+	 * @param left	linke Indexgrenze des zu sortierenden Arrays
+	 * @param right rechte Indexgrenze des zu sortierenden Arrays
+	 * @return - 
+	 * Effekt: ruft quicksort rekursiv auf bis die terminierungsbedingung erfüllt ist (Länge des Teilarrays beträgt 1) 
+	 ********************************************************************************************/
 	private static void quicksort(int[] array, int left, int right) {
-		// terminates the method if the array behind the pivot is only one
-		// element long
 		if (right - left <= 0)
 			return;
 		else {
@@ -29,50 +42,61 @@ public class Quicksort {
 			quicksort(array, left, pivot -1);
 			quicksort(array, pivot + 1, right);
 		}
-
 	}
 
-	// devide function returns the position of the pivot element. Everything
-	// behind the pivot element is smaller and everything after the pivot is
-	// larger than itself
+	
+	/********************************************************************************************
+	 * Methode devide wählt pivot point zum Vergleich an position array[right]
+	 * aus und durchsucht das Array von links nach Werten > array[pivot] und von
+	 * rechts > array[pivot] und ruft Methode swap auf.
+	 * @param array das zu sortierende array
+	 * @param left linker Marker des Teilarrays
+	 * @param  right rechter marker des Teilarrays
+	 * @return left	die endgültige Position des einsortierten Pivot Elements 
+	 * Effekt: Vergleicht Werte des Arrays mit einem Pivot Element und sortiert dieses an die korrekte Stelle ein
+	 ********************************************************************************************/
+	
 	private static int devide(int[] array, int left, int right) {
-		// choose pivot for comparison
 		int pivot = right;
-		
 
-		// infinite loop terminates when markers cross
 		while (true) {
-
-			// looks for an element larger than the pivot
+			// sucht ein Element, das größer ist als das Pivot Element.
 			while (left < pivot && array[left] <= array[pivot]) {
 				left++;
 			}
-			// looks for an element smaller than the pivot
+			// sucht ein Element, das kleiner ist als das Pivot Element
 			while (right >= left && array[right] > array[pivot]) {
 				right--;
 			}
-			// swaps the left element and the pivot and terminates the loop
+			//tauscht das linke Element und das Pivot Element aus, sobald sich die Marker überschneiden und beendet die Methode
 			if (right <= left) {
 				swap(array, left, pivot);
 				break;
 			}
-			// else swap values at left and right marker
+			// tauscht Elemente am linken und rechten Marker aus und führt die Methode weiter aus.
 			else {
 				swap(array, left, right);
 				left++;
 				right--;
 			}
 		}
-		// return the new pivot point
-
 		return left;
-
 	}
 
+	
+	/************************************************************************
+	 * Methode swap tauscht die Werte von array[left] und array[right] aus
+	 * @param array das gegebene Array
+	 * @param left	Index von einem der beiden zu tauschenden Elemente
+	 * @param right	Index von einem der beiden zu tauschenden Elemente
+	 * Vorbedingung -
+	 * @return -
+	 * Effekt Werte werden getauscht
+	 * 
+	*************************************************************************/	
 	private static void swap(int[] array, int left, int right) {
 		int tmp = array[left];
 		array[left] = array[right];
 		array[right] = tmp;
 	}
-
 }
