a47eed2f039d0b6edfce8044416612d33167003f
Michael Dinitz <michaelsmbp@Michaels-MBP.fritz.box>
2017-05-14 17:15:14 +0200

    Done

--- src/alg/a01/Test.java
+++ src/alg/a01/Test.java
@@ -3,72 +3,53 @@ package alg.a01;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class Test {
 
 	public static void main(String[] args) {
 		testFromFiles("./lib/");
-		
+
 	}
 	
-	public static void testFromFiles(String pathname)  {
+	/*****************************************************************************
+	 * Die Methode testFromFiles testet die sort Methode aus der Klasse
+	 * Quicksort und schreibt für jede Test Datei den File Namen und die
+	 * Laufzeit in ms in eine Datei test.txt
+	 * 
+	 * @param pathname Pfad des Orderns, in dem die Testdateien hinterlegt sind
+	 * @return -
+	 */
+
+	public static void testFromFiles(String pathname) {
 		try {
 			File dir = new File(pathname);
-			String [] files = dir.list();
+			String[] files = dir.list();
 			FileWriter fw = new FileWriter("./test/test.txt");
-			for(String file: files) {
-				fw.write(file+" "+testTimeFromFile("./lib/"+files[0]).toString()+" ms \n");
+			for (String file : files) {
+				fw.write(file + " " + testTimeFromFile("./lib/" + files[0]).toString() + " ms \n");
 			}
 			fw.close();
-		}
-		catch(IOException  e) {
+		} catch (IOException e) {
 			System.err.println("IOException");
 		}
-		
-		
-		
-	}
 
-	public static Long testTimeFromFile (String filename) {
-		int [] array= FileIntArray.FileToIntArray(filename);
+	}
+	/*****************************************************************************
+	 * erstellt ein int array mit der gegebenen testMethode FileIntArray und gibt die Zeit in ms zurück 
+	 * @param filename  Pfad der Datei, die getestet werden soll
+	 * @return testTime  Laufzeit in ms
+	 */
+
+	public static Long testTimeFromFile(String filename) {
+		int[] array = FileIntArray.FileToIntArray(filename);
 		return testTime(array);
-		
-	}
-	
-	public static void print(int[] array) {
-		PrintWriter out = new PrintWriter(System.out, true);
-		for (int i : array) {
-			out.print(i + " ");
-		}
-		out.println();
 
 	}
-
-	public static int[] createTestList(int size) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < size; i++) {
-			list.add((int) (Math.random() * 1000 + i));
-
-		}
-		int[] array = new int[list.size() - 1];
-		for (int j = 0; j < list.size(); j++) {
-			array[j] = list.get(j);
-		}
-		return array;
-	}
-
-	public static void test(int[] array) {
-		Quicksort.sort(array);
-		for (int i = 0; i < array.length - 1; i++) {
-			if (array[i] > array[i + 1]) {
-				System.out.println("error at " + i);
-			}
-		}
-		print(array);
-	}
-
+	/*****************************************************************************
+	 * Führt die Methode sort aus Quicksort aus und gibt die Laufzeit zurück
+	 * @param array das gegebene Array
+	 * @return elapsedTime die Laufzeit der Sortierung in ms
+	 */
 	public static long testTime(int[] array) {
 		long start = System.currentTimeMillis();
 		Quicksort.sort(array);
