d0718ae979256d5acf4e977d30f0719532675456
Michael Dinitz <michaelsmbp@Michaels-MacBook-Pro.local>
2017-06-03 14:51:03 +0200

    added a few test methods

--- src/alg/a02_1/Test.java
+++ src/alg/a02_1/Test.java
@@ -1,32 +1,49 @@
 package alg.a02_1;
 
-import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.ListIterator;
-
 public class Test {
 	
 
 	public static void main(String[] args) {
-		HashTable<String,String> table = new HashTable<String,String>(7);
-		table.put("hallo", "servus welt");
-		System.out.println(table.get("hallo"));
-		table.remove("hello");
-		System.out.println(table.get("hello"));
+		HashTable <Integer,String> table = new HashTable <Integer,String>(7);
+		testPut(table,10,"test");
+		testPut(table,10,"hello");
+		testPut(table,30,"something something to test");
+		testPut(table,30,"something something to test");
 
 	}
 
-	public static void print(HashTable<Integer,String> table) {
+	
+	/**
+	 * testet die put Methode der HashTable Klasse mit Integer als Key und String als Value
+	 * @param table	
+	 */
+	public static void testPut(HashTable<Integer,String> table,Integer Key,String Value) {
+		table.put(Key, Value);
+		if(isEqual(table,Key,Value)){
+			System.out.println("success");
+		}
+		else {
+			System.out.println("failed");
+		}
 		
 	}
 	
-	public static void printList(LinkedList<Pair<Integer,String>> list,PrintWriter out) {
-		ListIterator <Pair<Integer,String>> iterate = list.listIterator();
-		while(iterate.hasNext()) {
-			Pair<Integer,String> nextPair = iterate.next();
-			out.println(nextPair.getKey()+" "+nextPair.getValue());
+	/**
+	 * testet für gegebene key und value pairs,ob der key innerhalb einer
+	 * HashTable denselben Wert hat wie der durchgegebene Value
+	 * 
+	 * @param table	gegebene HashTable
+	 * @param Key	der Schlüssel nach dem der Wert abgefragt wird
+	 * @param Value	Der zu erwartende Wert
+	 * @return	boolean
+	 */
+	public static boolean isEqual(HashTable<Integer,String> table,Integer Key,String Value) {
+		if(table.get(Key).equals(Value)) {
+			return true;
 		}
-		out.close();
+		return false;
 	}
+	
+	
 
 }
